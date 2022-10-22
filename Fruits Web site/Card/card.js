a494573b7130d8ca9b02be74c933c46d31cd08ea
Ashim Devnath <89856438+Ashimdevnath@users.noreply.github.com>
2022-10-22 15:34:45 +0530

    WebSide Uploaded

---
+++ Fruits Web site/Card/card.js
@@ -0,0 +1,8 @@
+// function textchange(){
+// let btn = document.getElementsByClassName('order-btn').innerHTML;
+//     btn.style.text = 'Order Now'
+// }
+let btn = document.getElementsByClassName('order-btn');
+btn.addEventListener('mouseover',function run(){
+    alert('hello mouse')
+});
